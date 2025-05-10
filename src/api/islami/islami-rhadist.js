49df696396b827c124045118a44d16f50ea9b7a0
Danz-dev <danz124c@gmail.com>
2025-05-10 23:44:15 +0700

    Update islami-rhadist.js

--- src/api/islami/islami-rhadist.js
+++ src/api/islami/islami-rhadist.js
@@ -1,27 +1,27 @@
 const axios = require('axios');
 
 module.exports = function (app) {
-    async function randomHadits() {
+    async function fetchRandomHadits() {
         try {
             const { data } = await axios.get('https://api.myquran.com/v2/hadits/arbain/semua');
-            const datanya = data.data;
-            const randomData = datanya[Math.floor(Math.random() * datanya.length)];
+            const list = data.data;
+            const random = list[Math.floor(Math.random() * list.length)];
 
             return {
                 sumber: "Scraper Hadits Arbain",
-                nomor: randomData.no,
-                judul: randomData.judul,
-                teks_arab: randomData.arab,
-                terjemahan: randomData.indo
+                nomor: random.no,
+                judul: random.judul,
+                teks_arab: random.arab,
+                terjemahan: random.indo
             };
         } catch (error) {
-            throw new Error("Gagal mengambil data");
+            throw new Error("Gagal mengambil data hadits.");
         }
     }
 
-    app.get('/islami/hadits', async (req, res) => {
+    app.get('/islami/hadits/random', async (req, res) => {
         try {
-            const result = await randomHadits();
+            const result = await fetchRandomHadits();
             res.json({
                 status: true,
                 creator: "Danz-dev",
